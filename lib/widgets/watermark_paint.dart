bbe2c9d4bdb6e6843c793ad28947b99918f84e54
xshadowlegendx <lyheng0@gmail.com>
2023-03-10 13:20:12 +0700

    paint watermark text all over

--- lib/widgets/watermark_paint.dart
+++ lib/widgets/watermark_paint.dart
@@ -3,12 +3,22 @@ import 'package:flutter/material.dart' as fm;
 
 class WatermarkPaint extends fm.CustomPainter {
   final String text;
+  final double zoom;
+  final double width;
+  final double height;
 
-  const WatermarkPaint({ required this.text });
+  const WatermarkPaint({
+    required this.text,
+    required this.zoom,
+    required this.width,
+    required this.height,
+  });
 
   @override
   void paint(fm.Canvas canvas, fm.Size size) {
-    const textStyle = fm.TextStyle(color: fm.Colors.white, fontSize: 16);
+    const double gapSize = 16;
+
+    final textStyle = fm.TextStyle(color: fm.Colors.white, fontSize: (8 + 4) * (1 + zoom / 100));
 
     final textSpan = fm.TextSpan(text: text, style: textStyle);
 
@@ -19,7 +29,28 @@ class WatermarkPaint extends fm.CustomPainter {
 
     textPainter.layout();
 
-    textPainter.paint(canvas, const fm.Offset(16, 16));
+    final textSize = textPainter.size;
+
+    final middleOffsetSize = textSize.width / 2;
+
+    const int additionalSizeTextcount = 2;
+
+    final realWidth = textSize.width * additionalSizeTextcount + width + gapSize + middleOffsetSize;
+    final realHeight = textSize.height * additionalSizeTextcount + height + gapSize + middleOffsetSize;
+
+    final int colCount = (realWidth / textSize.height).round();
+    final int rowCount = (realHeight / textSize.width).round();
+
+    for (int rIdx = -additionalSizeTextcount; rIdx < rowCount; rIdx++) {
+      for (int cIdx = -additionalSizeTextcount; cIdx < colCount; cIdx++) {
+        textPainter.paint(
+          canvas, fm.Offset(
+            rIdx * (textSize.width + gapSize),
+            cIdx * (textSize.height + gapSize),
+          ),
+        );
+      }
+    }
   }
 
   @override
